ca7e8344b5faf1d788251b5d28af68590db90f4c
Piotr Kędziora <piterek007@o2.pl>
2019-04-11 01:41:05 +0200

    first commit

---
+++ airhelp.rb
@@ -0,0 +1,68 @@
+require 'json'
+
+class Message
+    attr_accessor :message, :tags, :words
+    def initialize (message, tags, words)
+        @message = message
+        @tags = tags
+        @words = words
+    end
+
+    def count_words
+        @words=@message.split
+    end
+
+    def tag_if_question
+        if !(@tags.include? "needs answer") and @message.include? "?"
+            @tags << "needs answer"
+        end
+    end
+
+    def as_json()
+        {message: @message, tags: @tags, words: @words}
+    end
+
+    def to_json()
+        as_json().to_json()
+    end
+end
+
+def get_json_input(directory)
+    json_string = File.read(directory, :encoding => 'utf-8')
+    text=JSON.parse(json_string)
+    mes = Message.new(text['message'], text['tags'], text['words']) 
+end
+
+if ARGV.size()<1 then
+    puts "please give at least one directory"
+end
+
+raw_input = []
+number_of_dirs = ARGV.size()
+
+for i in 0..number_of_dirs-1
+    #read source
+    begin
+        if ARGV[i].end_with? ".json"
+            raw_input<<get_json_input(ARGV[i])
+        else 
+            raise ArgumentError.new("It must be .json file!")
+        end
+    rescue Errno::ENOENT
+        puts 'No such file or directory. Try again'
+        exit
+    rescue ArgumentError => e
+        puts e.message
+        exit
+        
+    end
+
+    #process message
+    raw_input[i].count_words
+    raw_input[i].tag_if_question
+
+    #save results
+    File.open(ARGV[i],"w") do |f|
+        f.write(raw_input[i-1].to_json)
+    end
+end
